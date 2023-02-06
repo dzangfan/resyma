3752b28453b85f6da89be52915e2dea20b4d35e1
Li Dzangfan <dzangfan@onmail.com>
2023-02-06 20:27:06 +0800

    Add type annotation for depth-first traversal

--- lib/resyma/core/parsetree/traversal.rb
+++ lib/resyma/core/parsetree/traversal.rb
@@ -3,6 +3,13 @@ require "resyma/core/parsetree/definition"
 module Resyma
   module Core
     class ParseTree
+      #
+      # Depth-firstly traverse the tree
+      #
+      # @yieldparam [Resyma::Core::ParseTree] A parse tree
+      #
+      # @return [nil] Nothing
+      #
       def depth_first_each(&block)
         yield self
 
@@ -11,6 +18,8 @@ module Resyma
         @children.each do |child|
           child.depth_first_each(&block)
         end
+
+        nil
       end
     end
   end
