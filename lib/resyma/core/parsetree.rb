77cf1fcbd62457937ed649a30250a6dacef1f854
Li Dzangfan <dzangfan@onmail.com>
2023-02-06 14:28:05 +0800

    Rename conversion.rb to converter.rb

--- lib/resyma/core/parsetree.rb
+++ lib/resyma/core/parsetree.rb
@@ -1,6 +1,6 @@
 require "resyma/core/parsetree/source"
 require "resyma/core/parsetree/definition"
 require "resyma/core/parsetree/builder"
-require "resyma/core/parsetree/conversion"
+require "resyma/core/parsetree/converter"
 
 __END__
